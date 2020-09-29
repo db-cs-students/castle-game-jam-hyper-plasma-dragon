ba248f19b1e0c2f73873118d3c0ad4c6f821ec6a
brokenpandalang <71138342+brokenpandalang@users.noreply.github.com>
2020-09-29 10:35:49 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -4,3 +4,28 @@ Creators: Noah Martinez, Raymond Waffle
 Description:hi 
 """
 # Setup game
+info.set_score(0)
+info.set_life(1)
+
+# Player
+my_sprite = sprites.create(img("""
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+"""))
+
+# player movement
+controller.player1.move_sprite(None)
--- main.ts
+++ main.ts
@@ -4,3 +4,27 @@ Creators: Noah Martinez, Raymond Waffle
 Description:hi 
 
  */
+//  Setup game
+info.setScore(0)
+info.setLife(1)
+//  Player
+let my_sprite = sprites.create(img`
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . 2 2 2 2 . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+    . . . . . . . . . . . . . . . .
+`)
+//  player movement
+controller.player1.moveSprite(null)
